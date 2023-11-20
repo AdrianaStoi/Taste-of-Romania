906f5d55c17ee0494c50a9cf22f18e89e2c09c98
Adriana Stoichita <minciuna_adriana@yahoo.com>
2023-11-20 12:43:01 +0000

    Add cache control

--- taste_of_romania/settings.py
+++ taste_of_romania/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'home',
 
     # Other
-    'storages'
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -160,6 +160,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+        # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'taste-of-romania-5dd5a29030c4'
     AWS_S3_REGION_NAME = 'eu-central-1'
