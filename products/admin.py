00ed23ad179dd37dbb42ad0a293a2a9cfedabf85
Adriana Stoichita <minciuna_adriana@yahoo.com>
2023-11-20 16:20:43 +0000

    Add product and category to admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    This class allows admin to manage products on the admin panel.
+    """
+    list_display = ('name', 'SKU', 'category','price','slug', 
+                    'created_on')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('created_on', 'name')
+    search_fields = ['name', 'description', 'category']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    This class allows admin to manage Category on the admin panel.
+    It displays the category name, creation date and update date.
+    """
+    list_display = ('name', 'created_on', 'updated_on')
