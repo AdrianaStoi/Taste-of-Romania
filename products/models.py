56c3b2aebf391066839aa0ca826238503a5dbfc3
Adriana Stoichita <minciuna_adriana@yahoo.com>
2023-11-22 22:01:18 +0000

    Add sizes to product model and size selector dropdown to product template

--- products/models.py
+++ products/models.py
@@ -29,6 +29,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     excerpt = models.TextField(blank=True)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     ingredients = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
