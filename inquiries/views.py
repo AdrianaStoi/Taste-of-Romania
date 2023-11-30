fdb047d6038a99dc869350761612c292a954958c
Adriana Stoichita <minciuna_adriana@yahoo.com>
2023-11-30 22:06:14 +0000

    Update inquiry view to display all user inquiries

--- inquiries/views.py
+++ inquiries/views.py
@@ -4,23 +4,16 @@ from .forms import InquiryForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.views import generic
 
-
-
-class AllInquiries(generic.ListView):
-    model = Inquiry
-    inquiries = Inquiry.objects.all()
-    template_name = 'inquiries/inquiries.html'
-    context_object_name = 'inquiries'
 
 @login_required
 def user_inquiry(request):
-    inquiry = None
+
+    inquiries = Inquiry.objects.filter(user=request.user).order_by('-created_on')
 
     if request.method == 'POST':
-        form =InquiryForm(request.POST)
-        if form.is_valid:
+        form = InquiryForm(request.POST)
+        if form.is_valid():
             inquiry = form.save(commit=False)
             inquiry.user = request.user
             inquiry.save()
@@ -28,5 +21,5 @@ def user_inquiry(request):
             return redirect(request.path)
     else:
         form = InquiryForm()
-    
-    return render(request, 'inquiries/inquiries.html', {'inquiry': inquiry, 'form': form})
+
+    return render(request, 'inquiries/inquiries.html', {'form': form, 'inquiries': inquiries})
